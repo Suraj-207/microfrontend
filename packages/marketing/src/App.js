834c70f5f603742efcd2489f9aa878974af33075
Suraj <sksuraj148333@gmail.com>
2021-09-19 11:59:37 +0530

    text changed in marketing deployment

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, BrowserRouter, Route} from 'react-router-dom'
 import {StylesProvider} from '@material-ui/core/styles'
-
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
