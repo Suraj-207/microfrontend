55dfb42da20343972ec908110d357fded1ba3fad
Suraj <sksuraj148333@gmail.com>
2021-09-20 22:40:30 +0530

    auth logic added

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
+
+//Mount function to start up the app
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDom.render(<App onSignIn={onSignIn} history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+//If we are in development and isolation call mount function immediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_auth-dev-root");
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+//Application is running through container so we should export the mount function
+export { mount };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,18 +1,37 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { createMemoryHistory, createBrowserHistory} from 'history';
 import App from "./App";
 
 //Mount function to start up the app
-const mount = (el) => {
-  ReactDom.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history = defaultHistory || createMemoryHistory({
+    initialPathEntries: [initialPath],
+  });
+  if(onNavigate){
+    history.listen(onNavigate);
+  }
+
+  ReactDom.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({pathname: nextPathname}) {
+      const { pathname } = history.location;
+      if(pathname !== nextPathname){
+        history.push(nextPathname);
+      }
+      
+    }
+  }
 };
 
+//If we are in development and isolation call mount function immediately
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot,{ defaultHistory: createBrowserHistory() });
   }
 }
 
-//If we are in development and isolation call mount function immediately
+//Application is running through container so we should export the mount function
 export { mount };
